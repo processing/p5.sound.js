8bbeec5774e3fd630e0ed988b978d3965cc9229c
tommy martinez <thomasjohnmartinez@gmail.com>
2024-06-06 00:34:41 -0400

    added output of the yuidoc . command to demonstrate oscillator reference page

--- out/api.js
+++ out/api.js
@@ -1,6 +1,8 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [],
+    "classes": [
+        "Oscillator"
+    ],
     "modules": [],
     "allModules": [],
     "elements": []

79ec287b845c4cbc4e96b71cddddccc1402a0413
tommy martinez <thomasjohnmartinez@gmail.com>
2024-06-25 12:10:46 -0400

    gain reference and example, gain amp method

--- src/Gain.js
+++ src/Gain.js
@@ -1,15 +1,60 @@
 import * as Tone from "tone";
 
 /**
- * Generate a gain node.
+ * Generate a gain node to use for mixing and main volume.
  * @class Gain
  * @constructor
+ * @example
+ * <div>
+ * <code>
+ * let cnv, soundFile, osc, gain;
  * 
+ * function preload() {
+ *   soundFile = loadSound('assets/Damscray_DancingTiger.mp3');
+ * }
+ * 
+ * function setup() {
+ *   cnv = createCanvas(100, 100);
+ *   cnv.mousePressed(playSound);
+ * 
+ *   gain = new Gain(0.74);
+ *   osc = new p5.Oscillator();
+ *   osc.amp(0.74);
+ *   osc.disconnect();
+ *   soundFile.loop();
+ *   soundFile.disconnect();
+ * 
+ *   //connect both sound sources to gain node
+ *   soundFile.connect(gain);
+ *   osc.connect(gain);
+ * }
+ * 
+ * function playSound() {
+ *   soundFile.play();
+ *   soundFile.play();
+ * }
+ * 
+ * function draw() {
+ *   background(220);
+ *   let level = map(mouseX, 0, width, 0, 1);
+ *   gain.amp(level);
+ * }
+ * </code>
+ * </div>
  */
 class Gain {
-  constructor(a) {
-    //proof constructor so that if you give string it will set type but if you give number it will set cutoff, if you give both it will set both
-    this.gain = new Tone.Gain(1).toDestination();
+  constructor(value = 1) {
+    this.gain = new Tone.Gain(value).toDestination();
+  }
+
+  /**
+   * Adjust the amplitude of the soundfile.
+   * @method amp
+   * @for Gain
+   * @param {Number} amplitude amplitude value between 0 and 1.
+   */
+  amp(value) {
+    this.gain.gain.rampTo(value, 0.1);
   }
 
   getNode() {

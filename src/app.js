3aeb6e74903d25a05a8d565e6d93bea42d068b66
tommy martinez <thomasjohnmartinez@gmail.com>
2024-09-19 00:15:57 -0400

    added p5 class prefix

--- src/app.js
+++ src/app.js
@@ -5,55 +5,55 @@ p5.prototype.userStartAudio = userStartAudio;
 p5.prototype.userStopAudio = userStopAudio;
 
 import Oscillator from './Oscillator';
-p5.prototype.Oscillator = Oscillator;
+p5.Oscillator = Oscillator;
 
 import {SawOsc, SinOsc, TriOsc, SqrOsc} from './Oscillator';
-p5.prototype.SawOsc = SawOsc;
-p5.prototype.SinOsc = SinOsc;
-p5.prototype.TriOsc = TriOsc;
-p5.prototype.SqrOsc = SqrOsc;
+p5.SawOsc = SawOsc;
+p5.SinOsc = SinOsc;
+p5.TriOsc = TriOsc;
+p5.SqrOsc = SqrOsc;
 
 import Envelope from './Envelope';
-p5.prototype.Envelope = Envelope;
+p5.Envelope = Envelope;
 
 import Delay from './Delay';
-p5.prototype.Delay = Delay;
+p5.Delay = Delay;
 
 import Reverb from './Reverb';
-p5.prototype.Reverb = Reverb;
+p5.Reverb = Reverb;
 
 import Biquad from './Biquad';
-p5.prototype.Biquad = Biquad;
+p5.Biquad = Biquad;
 
 import PitchShifter from './PitshShifter';
-p5.prototype.PitchShifter = PitchShifter;
+p5.PitchShifter = PitchShifter;
 
 import Gain from './Gain';
-p5.prototype.Gain = Gain;
+p5.Gain = Gain;
 
 import Amplitude from './Amplitude';
-p5.prototype.Amplitude = Amplitude;
+p5.Amplitude = Amplitude;
 
 import FFT from './FFT';
-p5.prototype.FFT = FFT;
+p5.FFT = FFT;
 
 import Noise from './Noise';
-p5.prototype.Noise = Noise;
+p5.Noise = Noise;
 
 import Panner from './Panner';
-p5.prototype.Panner = Panner;
+p5.Panner = Panner;
 
 import Panner3D from './Panner3D';
-p5.prototype.Panner3D = Panner3D;
+p5.Panner3D = Panner3D;
 
 p5.prototype.loadSound = loadSound;
 p5.prototype.registerPreloadMethod('loadSound', p5.prototype);
 
 import SoundFile, { loadSound } from './SoundFile';
-p5.prototype.SoundFile = SoundFile;
+p5.SoundFile = SoundFile;
 
 import AudioIn from './AudioIn';
-p5.prototype.AudioIn = AudioIn;
+p5.AudioIn = AudioIn;
 
 //import Recorder from './Recorder';
 //p5.prototype.Recorder = Recorder;

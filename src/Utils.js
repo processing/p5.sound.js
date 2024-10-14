b0587449bf93a9f09779476b9762a7ab2308ad6d
tommy martinez <thomasjohnmartinez@gmail.com>
2024-10-14 17:08:30 -0400

    added documentation for AudioContext utils

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,9 @@
+/**
+ *  @module p5.sound
+ *  @submodule p5.sound
+ *  @for p5.sound
+ */
+
 import { getContext as ToneGetContext, setContext as ToneSetContext } from "tone/build/esm/core/Global.js";
 import { start as ToneStart } from "tone/build/esm/core/Global.js";
 
@@ -5,19 +11,37 @@ function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
 }
 
+/**
+ *  Get the window's audio context.
+ *  @function getAudioContext
+ *  @return {AudioContext} the audio context
+ */
 function getAudioContext() {
     let context = ToneGetContext();
     return context;
 }
 
+/**
+ *  Sets the AudioContext to a specified context to enable cross library compatibility.
+ *  @function setAudioContext
+ *  @param {AudioContext} the desired AudioContext.
+ */
 function setAudioContext(context) {
     ToneSetContext(context);
 }
 
+/**
+ *  userStartAudio() starts the AudioContext on a user gesture. It can be placed in a specific interaction function, such as mousePressed().
+ *  @function userStartAudio
+ */
 function userStartAudio() {
     ToneStart();
 }
 
+/**
+ *  userStopAudio() stops the AudioContext on a user gesture.
+ *  @function userStopAudio
+ */
 function userStopAudio() {
     context = ToneGetContext();
     context.suspend();

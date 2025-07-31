12fec874db74597bc8be6a4a43e057a67dea556e
paulparoczai <paulparoczai@gmail.com>
2025-07-30 18:21:52 -0700

    update app.js imports

--- src/app.js
+++ src/app.js
@@ -1,63 +1,70 @@
-import { getAudioContext, setAudioContext, userStartAudio, userStopAudio } from './Utils';
+import { P5SoundContext } from "./P5SoundContext.js";
 
-p5.prototype.getAudioContext = getAudioContext;
-p5.prototype.setAudioContext = setAudioContext;
-p5.prototype.userStartAudio = userStartAudio;
-p5.prototype.userStopAudio = userStopAudio;
+p5.prototype.getAudioContext = P5SoundContext.getAudioContext;
+p5.prototype.setAudioContext = P5SoundContext.setAudioContext;
+p5.prototype.userStartAudio = P5SoundContext.userStartAudio;
+p5.prototype.userStopAudio = P5SoundContext.userStopAudio;
 
-import { P5SoundOscillator }  from './nodes/core/source/P5SoundOscillator.js';
+import { P5SoundOscillator }  from './nodes/source/oscillator/P5SoundOscillator.js';
 p5.Oscillator = P5SoundOscillator;
 
-import { P5SoundSawOsc, P5SoundSinOsc, P5SoundTriOsc, P5SoundSqrOsc } from './nodes/core/source/P5SoundOscillator';
+import { P5SoundSawOsc } from './nodes/source/oscillator/P5SoundSawOsc.js';
+import { P5SoundSinOsc } from './nodes/source/oscillator/P5SoundSinOsc.js';
+import { P5SoundTriOsc } from './nodes/source/oscillator/P5SoundTriOsc.js';
+import { P5SoundSqrOsc } from './nodes/source/oscillator/P5SoundSqrOsc.js';
+
 p5.SawOsc = P5SoundSawOsc;
 p5.SinOsc = P5SoundSinOsc;
 p5.TriOsc = P5SoundTriOsc;
 p5.SqrOsc = P5SoundSqrOsc;
 
-import Envelope from './Envelope';
-p5.Envelope = Envelope;
+import { P5SoundEnvelope } from './nodes/effect/P5SoundEnvelope.js';
+p5.Envelope = P5SoundEnvelope;
 
-import { P5SoundDelay } from './nodes/core/effect/P5SoundDelay.js';
+import { P5SoundDelay } from './nodes/effect/P5SoundDelay.js';
 p5.Delay = P5SoundDelay;
 
-import Reverb from './Reverb';
-p5.Reverb = Reverb;
+import { P5SoundReverb } from './nodes/effect/P5SoundReverb.js';
+p5.Reverb = P5SoundReverb;
 
-import { P5SoundBiquad } from './nodes/core/effect/P5SoundBiquad.js';
+import { P5SoundBiquad } from './nodes/effect/biquad/P5SoundBiquad.js';
 p5.Biquad = P5SoundBiquad;
 
-import { P5SoundLowPass, P5SoundHighPass, P5SoundBandPass } from './nodes/core/effect/P5SoundBiquad';
+import { P5SoundLowPass } from './nodes/effect/biquad/P5SoundLowPass.js';
+import { P5SoundHighPass } from './nodes/effect/biquad/P5SoundHighPass.js';
+import { P5SoundBandPass } from './nodes/effect/biquad/P5SoundBandPass.js';
+
 p5.LowPass = P5SoundLowPass;
 p5.HighPass = P5SoundHighPass;
 p5.BandPass = P5SoundBandPass;
 
-import PitchShifter from './PitchShifter';
-p5.PitchShifter = PitchShifter;
+import { P5SoundPitchShifter } from './nodes/effect/P5SoundPitchShifter.js';
+p5.PitchShifter = P5SoundPitchShifter;
 
-import { P5SoundGain } from './nodes/core/effect/P5SoundGain.js';
+import { P5SoundGain } from './nodes/effect/P5SoundGain.js';
 p5.Gain = P5SoundGain;
 
-import Amplitude from './Amplitude';
-p5.Amplitude = Amplitude;
+import { P5SoundAmplitude } from './nodes/analysis/P5SoundAmplitude.js';
+p5.Amplitude = P5SoundAmplitude;
 
-import FFT from './FFT';
-p5.FFT = FFT;
+import { P5SoundFFT } from './nodes/analysis/P5SoundFFT.js';
+p5.FFT = P5SoundFFT;
 
-import Noise from './Noise';
-p5.Noise = Noise;
+import { P5SoundNoise } from './nodes/source/P5SoundNoise.js';
+p5.Noise = P5SoundNoise;
 
-import { P5SoundPanner } from './nodes/core/effect/P5SoundPanner.js';
+import { P5SoundPanner } from './nodes/effect/P5SoundPanner.js';
 p5.Panner = P5SoundPanner;
 
-import Panner3D from './Panner3D';
-p5.Panner3D = Panner3D;
+import { P5SoundPanner3D } from './nodes/effect/P5SoundPanner3D';
+p5.Panner3D = P5SoundPanner3D;
 
-import SoundFile, { loadSound } from './SoundFile';
-p5.SoundFile = SoundFile;
+import { P5SoundFile, loadSound } from './nodes/source/P5SoundFile.js';
+p5.SoundFile = P5SoundFile;
 p5.prototype.loadSound = loadSound;
 
-import AudioIn from './AudioIn';
-p5.AudioIn = AudioIn;
+import { P5SoundAudioIn } from './nodes/source/P5SoundAudioIn.js';
+p5.AudioIn = P5SoundAudioIn;
 
 p5.AudioOutput = new p5.Gain();
 p5.AudioOutput.toDestination();

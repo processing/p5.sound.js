92562116ad53c0963d2e9115cc4e60cf3945bcc2
tommy martinez <thomasjohnmartinez@gmail.com>
2025-10-26 13:50:01 -0400

    documentation for p5soundMixEffect nodes

--- src/core/p5soundMixEffect.js
+++ src/core/p5soundMixEffect.js
@@ -26,6 +26,34 @@ class p5soundMixEffect extends p5soundNode {
    * @example
    * <div>
    * <code>
+   * let osc, del
+   *
+   * function setup() {
+   *   cnv = createCanvas(100, 100);
+   *   background(220);
+   *   osc = new p5.Oscillator();
+   *   osc.disconnect();
+   *   del = new p5.Delay();
+   *   osc.connect(del);
+   *   del.delayTime(0.25);
+   *   del.feedback(0.5);
+   *   del.wet(0.5);
+   * }
+   *
+   * function mousePressed() {
+   *   osc.start();
+   * }
+   *
+   * function draw() {
+   *   del.delayTime(map(mouseX, 0, width, 0.01, 0.5));
+   *   del.wet(map(mouseY, 0, height, 0.1, 0.9));
+   *   background(220);
+   *   textAlign(CENTER);
+   *   textSize(9);
+   *   text('delay dry/wet: ' + del.wet().toFixed(2), width / 2, height / 2);
+   * }
+   * </code>
+   * </div>
    */
   wet(amount) {
     if (typeof amount !== 'undefined') {

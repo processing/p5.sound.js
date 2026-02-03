7e7223058e526b9abd1747fd44e825253b6fcc56
tommy martinez <thomasjohnmartinez@gmail.com>
2026-02-03 11:48:10 -0500

    add friendly description for p5soundNode class

--- src/core/p5soundNode.js
+++ src/core/p5soundNode.js
@@ -7,7 +7,9 @@
 import { Context as ToneContext } from "tone/build/esm/core/context/Context.js";
 
 /**
- * Generic methods for p5.sound.js nodes
+ * This is the primary or "base" class for p5.sound.js nodes.
+ * 
+ * It allows p5.sound.js audio sources and effects to connect to one another. It also allows you to modify their volumes. 
  * @class p5soundNode
  * @constructor
  */
@@ -17,7 +19,9 @@ class p5soundNode {
   }
 
   /**
-   * Adjust the amplitude of the p5 sound node.
+   * Adjust the amplitude of the p5 sound node. 
+   * 
+   * Amplitude is another way of saying "volume" or "loudness."
    * @method amp
    * @for p5soundNode
    * @param {Number} amplitude Set the amplitude between 0 and 1.0. Or, pass in an object such as an oscillator to modulate amplitude with an audio signal.
@@ -66,6 +70,50 @@ class p5soundNode {
     this.node.volume.rampTo(dbValue, p);
   }
 
+  /**
+   * Connects audio nodes together.  
+   * 
+   * You can connect a node to multiple destinations simultaneously by calling this method multiple times.
+   * @method connect
+   * @for p5soundNode
+   * @param {Object} destination The node you would like to connect to.
+   * @example
+   * <div>
+   * <code>
+   * let osc, delay;
+   * let cnv;
+   * 
+   * function setup() {
+   *   describe("a sketch that demonstrates how to connect an audio source node to an effect node");
+   *   cnv = createCanvas(100, 100);
+   *   cnv.mousePressed(startSound);
+   *   textAlign(CENTER);
+   *   textWrap(WORD);
+   *   textSize(10);
+   *   
+   *   osc = new p5.Oscillator('sine');
+   *   delay = new p5.Delay(0.120, 0.65);
+   *   //disconnect the oscillator from the speakers before connecting to the delay effect!
+   *   osc.disconnect();
+   *   //connect the oscillator to the delay effect.
+   *   osc.connect(delay);
+   * }
+   * 
+   * function startSound() {
+   *   osc.start();
+   * }
+   * 
+   * function draw(){
+   *   background(220);
+   *   text('click to play sound', 0, height/2 - 20, 100);
+   *   text('control oscillator frequency with mouseX position', 0, height/2, 100);
+   * 
+   *   let freq = map(mouseX, 0, width, 800, 1600);
+   *   osc.freq(freq);
+   * }
+   * </code>
+   * </div>
+   */
   connect(destination) {
     if(typeof destination.getNode === 'function') {
       this.node.connect(destination.getNode());
@@ -74,10 +122,22 @@ class p5soundNode {
     }
   }
   
+  /**
+   * Disconnect an audio node from the main output.
+   * 
+   * You may want to disconnect your audio source from the main output before you connect it to another effect. It is used in many of the p5.sound.js examples.
+   * @method disconnect
+   * @for p5soundNode
+   */
   disconnect() {
     this.node.disconnect(ToneContext.destination);
   }
 
+  /**
+   * A private function that is called when an audio source tries to connect to this node.  
+   * @method getNode
+   * @for p5soundNode
+   */
   getNode() {
     return this.node;
   }
